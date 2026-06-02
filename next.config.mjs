03e2766b884f76999fd54006a3431ccbd823ffef
旷路 <mashengyuan.msy@alibaba-inc.com>
2026-06-02 10:13:21 +0800

    test delete image in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  images: {
-    loader: 'custom',
-    // `pcg build` 启动时把 @ali/pcg-cli 自带的 loader 复制到
-    // node_modules/.pcg/ 下,项目根 / package.json 都不需要改。
-    loaderFile: 'node_modules/.pcg/pcg-image-loader.js',
-  },
+  // images: {
+  //   loader: 'custom',
+  //   // `pcg build` 启动时把 @ali/pcg-cli 自带的 loader 复制到
+  //   // node_modules/.pcg/ 下,项目根 / package.json 都不需要改。
+  //   // loaderFile: 'node_modules/.pcg/pcg-image-loader.js',
+  // },
 };
 
 export default nextConfig;
